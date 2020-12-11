22978eff0ab337cb8254cde29f5c8a9267b17d1b
Linkin Ben <linkinben@localhost.localdomain>
2020-12-11 13:06:33 +0700

    Fix Stuff

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express Motherfucker' });
+  res.render('index', { title: 'Welcome To My Application' });
 });
 
 module.exports = router;
