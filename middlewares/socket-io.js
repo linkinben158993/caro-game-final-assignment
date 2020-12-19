c7f07215e4a0581f2ab17b03e0cf67054e1800ca
Linkin Ben <linkinben@localhost.localdomain>
2020-12-19 11:24:47 +0700

    Response Resolved

--- middlewares/socket-io.js
+++ middlewares/socket-io.js
@@ -48,9 +48,9 @@ module.exports = {
       });
 
       socket.on('create-room', (response) => {
-        console.log(response);
         response.guests = [];
         activeRooms.push(response);
+        console.log(response);
         socket.emit('created-room', response);
 
         // socket.join(response.roomId);
@@ -63,39 +63,43 @@ module.exports = {
 
       socket.on('joined', (response) => {
         const roomJoinedIndex = activeRooms.map((id) => id.roomId).indexOf(response.roomId);
-        if (activeRooms[roomJoinedIndex].y !== null) {
+        if (activeRooms[roomJoinedIndex].y === null) {
           activeRooms[roomJoinedIndex].y = {
             username: response.currentUser.user.email,
             fullName: response.currentUser.user.fullName,
           };
+
+          // console.log(activeRooms[roomJoinedIndex]);
+          // const newMatch = new Matches({
+          //   matchId: activeRooms[roomJoinedIndex].roomId,
+          //   host: activeRooms[roomJoinedIndex].x.username,
+          //   opponent: activeRooms[roomJoinedIndex].y.username,
+          //   match: {
+          //     moves: [],
+          //   },
+          //   chatLogs: [],
+          //   // -1: Left room Left Room Close Browser, 0: Unfinished (), 1: Complete
+          //   status: 0,
+          // });
+          // newMatch.save().then(() => {
+          //   console.log('Save new game');
+          //   io.emit('player-join-game', {
+          //     roomId: response.roomId,
+          //     roomDetails: activeRooms[roomJoinedIndex],
+          //     // eslint-disable-next-line no-underscore-dangle
+          //     matchId: newMatch._id,
+          //   });
+          // });
+          io.emit('player-join-game', {
+            roomId: response.roomId,
+            roomDetails: activeRooms[roomJoinedIndex],
+          });
         } else {
           activeRooms[roomJoinedIndex].guests.push({
             username: response.currentUser.user.email,
             fullName: response.currentUser.user.fullName,
           });
         }
-
-        // console.log(activeRooms[roomJoinedIndex]);
-        // const newMatch = new Matches({
-        //   matchId: activeRooms[roomJoinedIndex].roomId,
-        //   host: activeRooms[roomJoinedIndex].x.username,
-        //   opponent: activeRooms[roomJoinedIndex].y.username,
-        //   match: {
-        //     moves: [],
-        //   },
-        //   chatLogs: [],
-        //   // -1: Left room Left Room Close Browser, 0: Unfinished (), 1: Complete
-        //   status: 0,
-        // });
-        // newMatch.save().then(() => {
-        //   console.log('Save new game');
-        //   io.emit('player-join-game', {
-        //     roomId: response.roomId,
-        //     roomDetails: activeRooms[roomJoinedIndex],
-        //     // eslint-disable-next-line no-underscore-dangle
-        //     matchId: newMatch._id,
-        //   });
-        // });
       });
 
       socket.on('request-start-game', (response) => {
