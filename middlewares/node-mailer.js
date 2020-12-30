17e8784fd4f68b7701d1460b067aa4fa8f979f1e
Linkin Ben <linkinben@localhost.localdomain>
2020-12-31 00:51:13 +0700

    Remove XOAuth2

--- middlewares/node-mailer.js
+++ middlewares/node-mailer.js
@@ -1,21 +1,21 @@
 const nodeMailer = require('nodemailer');
 const dotenv = require('dotenv');
-const xoauth2 = require('xoauth2');
 
 dotenv.config();
 
 const email = process.env.nodeMailerEmail;
+const password = process.env.nodeMailerPassword;
 
 const transporter = nodeMailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
-    xoauth2: xoauth2.createXOAuth2Generator({
-      user: email,
-      clientId: process.env.nodeMailerClId,
-      clientSecret: process.env.nodeMailerSecret,
-    }),
+    user: email,
+    pass: password,
+    clientId: process.env.nodeMailerClId,
+    clientSecret: process.env.nodeMailerSecret,
+    refreshToken: process.env.nodeMailerRefreshToken,
   },
   tls: {
     rejectUnauthorized: false,
@@ -32,18 +32,16 @@ module.exports = {
       text:
         `Nhập mã OTP tương ứng để kích hoạt email của bạn: ${otp} \n`
         + 'Vui lòng không cung cấp OTP này ch ai khác!',
-      auth: {
-        user: email,
-      },
     };
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-          console.log('Error', error);
-          reject({ success: false, error });
+          console.log('Error: ', error);
+          resolve({ success: false, error });
+        } else {
+          console.log('Info:  ', info);
+          resolve({ success: true, info });
         }
-        console.log('Info', info);
-        resolve({ success: true, info });
       });
     });
   },
