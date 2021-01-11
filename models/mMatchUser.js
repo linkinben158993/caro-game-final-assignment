0bd9d92a3842ce2d123cd16ac9e06b57ea32f9a7
Linkin Ben <linkinben@localhost.localdomain>
2021-01-12 00:17:47 +0700

    More Info On Users Stats

--- models/mMatchUser.js
+++ models/mMatchUser.js
@@ -6,21 +6,37 @@ module.exports = {
     let foundUser;
     try {
       foundUser = await Users.findOne({ _id: userId });
+      const createdTimestamp = foundUser._id.getTimestamp().getTime();
+
       Matches.getMatchByUserId(userId, (err, document) => {
         if (err) {
           return callBack(err);
         }
-        const createdTimestamp = foundUser._id.getTimestamp().getTime();
+
         foundUser.createdTimeStamp = createdTimestamp;
-        if (document === null || foundUser === null) {
-          return callBack(null, 0);
+        if (document === 0 || foundUser === null) {
+          return callBack(null, {
+            user: {
+              email: foundUser.email,
+              fullName: foundUser.fullName,
+              joinTimeStamp: createdTimestamp,
+            },
+            stats: {
+              playedGame: 0,
+              winRate: 0,
+            },
+          });
         }
 
         const sumUserMatches = document.length;
         const userWonMatches = document.filter((data) => data.winner === foundUser.email).length;
 
         return callBack(null, {
-          user: { email: foundUser.email, fullName: foundUser.fullName },
+          user: {
+            email: foundUser.email,
+            fullName: foundUser.fullName,
+            joinTimeStamp: createdTimestamp,
+          },
           stats: {
             playedGame: sumUserMatches,
             winRate: Math.round((userWonMatches / sumUserMatches) * 100),
